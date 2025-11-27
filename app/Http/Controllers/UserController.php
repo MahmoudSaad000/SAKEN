3155fadfbebe9e3ff0bad2a375d4984f2e506ba8
Mahmoud Saad <wwwmahmoud.sa55@gmail.com>
2025-11-28 00:06:24 +0300

    this is a project

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    function index()
+    {
+        $Users = [
+            ["id" => 1, "Name" => "Mahmoud"],
+            ["id" => 2, "Name" => "Ahmad"],
+            ["id" => 3, "Name" => "Maher"]
+        ];
+
+        //foreach ($Users as $User) {
+        //  echo $User['id'] . ',' . $User['Name'] . "\n" ;
+        //}
+        return response()->json($Users);
+    
+    }
+
+    public function CheckUser($id)
+    {
+
+        if ($id > 10) {
+            return response()->json(['Message' => 'Accesse Denied']);
+        } else {
+            return response()->json(['Message'=>'Your id is Valid']);
+        }
+    }
+    
+}
---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Welcomecontroller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::get('/Welcome', [Welcomecontroller::class, 'Welcome']);
+Route::get('/User', [UserController::class, 'index']);
+
+
+Route::get('User/{id}', [UserController::class, 'CheckUser']);

40503eee17d1c295ebacff3b520388576a66b186
Abdulrahman Al-Khuraki <abdulrahmanalkhuraki@gmail.com>
2025-12-02 16:13:19 +0300

    edit api file

--- routes/api.php
+++ routes/api.php
@@ -1,38 +1,37 @@
 <?php
 
+use App\Http\Controllers\ApartmentController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-
+// Public routes
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
 
+// Admin-only route
+Route::get('users', [UserController::class, 'getAllUsers'])->middleware('isAdmin');
 
-Route::middleware('auth:sanctum')->group(function () {
+// Bookings routes
+Route::prefix('bookings')->group(function () {
 
-    Route::get('users', [UserController::class, 'getAllUsers'])->middleware('isAdmin');
-
-
-    Route::prefix('/bookings')->group(function () {
-        Route::middleware('isRenter')->group(function () {
-            Route::apiResource('', BookingController::class);
-            Route::put('{booking}/rate', [BookingController::class, 'rateBooking']);
-        });
-
-
-    Route::prefix('/bookings')->group(function () {
-        Route::middleware('isAdmin')->group(function () {
-
-            Route::get('/all', [BookingController::class, 'getAllBookings'])->middleware('isAdmin');
-        });
-
-        Route::middleware('isOwner')->group(function () {
-
-            Route::get('/unconfirmed', [BookingController::class, 'getUnConfirmedBookings']);
-            Route::put('/{booking_id}/confirm', [BookingController::class, 'confirmBooking']);
-        });
+    // Renter routes
+    Route::middleware('isRenter')->group(function () {
+        Route::apiResource('', BookingController::class);
+        Route::put('{booking}/rate', [BookingController::class, 'rateBooking']);
     });
+
+    // Admin routes
+    Route::middleware('isAdmin')->group(function () {
+        Route::get('all', [BookingController::class, 'getAllBookings']);
+    });
+
+    // Owner routes
+    Route::middleware('isOwner')->group(function () {
+        Route::get('unconfirmed', [BookingController::class, 'getUnConfirmedBookings']);
+        Route::put('{booking_id}/confirm', [BookingController::class, 'confirmBooking']);
+    });
+
+    Route::apiResource('apartment',ApartmentController::class);
+
 });
